12958d11b61cf3ad3e9301520c68abd5574222df
Androto9 <71312332+Androto9@users.noreply.github.com>
2022-02-02 15:32:48 +0100

    auditor done

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -3,6 +3,7 @@
 const protocol = require('./orchestra-protocol');
 const dgram = require('dgram');
 const s = dgram.createSocket('udp4');
+const net = require('net');
 
 const soundFromInstrument = new Map();
 soundFromInstrument.set('ti-ta-ti', 'piano');
@@ -11,33 +12,78 @@ soundFromInstrument.set('trulu', 'flute');
 soundFromInstrument.set('gzi-gzi', 'violin');
 soundFromInstrument.set('boum-boum', 'drum');
 
-var musicians = [];
+let musicians = [];
 
-function updateMusicianTime() {
+function updateMusiciansTime() {
     musicians.forEach(function (musician) {
         musician.time++;
     });
 }
 
 function clearMusicians() {
-    for (var i = 0; i < musicians.length) {
+    for (let i = 0; i < musicians.length; ++i) {
         if (musicians[i].time > 5) {
             musicians.splice(i, 1);
         }
     }
 }
 
+// Join the multicast UDP address
 s.bind(protocol.PROTOCOL_PORT, function () {
     console.log("Joining multicast group");
     s.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
 });
 
+// Get a sound when it arrives on the multicast address
 s.on('message', function (msg, source) {
     console.log("Data has arrived: " + msg + ". Source port: " + source.port);
 
-    var data = JSON.parse(msg);
+    // Parses the JSON data
+    let data = JSON.parse(msg);
 
+    let alreadyPlaying = false;
 
+    // Check if a musician is already in the list
+    musicians.forEach(function (musician) {
+        if (musician.uuid === data.uuid) {
+            alreadyPlaying = true;
+        }
+    });
+
+    // Add the musician in the list if not there yet or update musician time
+    if (!alreadyPlaying) {
+
+        let musician = {
+            uuid: data.uuid,
+            instrument: soundFromInstrument.get(data.sound),
+            activeSince: new Date(Date.now()).toISOString(),
+            time: 0
+        };
+
+        musicians.push(musician);
+
+    } else {
+
+        let updateTime = musicians.indexOf(data.uuid);
+        updateTime.time = 0;
+    }
 });
 
-setInterval();
+var tcpServer = net.createServer(function (socket) {
+    console.log('Client connected to TCP server');
+
+    clearMusicians();
+
+    let musiciansList = JSON.parse(JSON.stringify(musicians));
+
+    musiciansList.forEach(function (this) { delete this.time })
+
+    socket.write(JSON.stringify(musicians));
+    socket.pipe(socket);
+    socket.destroy();
+});
+
+tcpServer.listen(protocol.PROTOCOL_TCP_PORT);
+
+
+setInterval(function () { updateMusiciansTime(); }, 1000);
